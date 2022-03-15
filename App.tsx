09679efeda5a3fb33f7351f2b22051346fdd0ed4
LeandroRMachado <leandroskullofwar@outlook.com>
2022-03-14 23:24:52 -0300

    first Commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading';
+
+import { StatusBarHeader } from './src/components/StatusBarHeader';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <>
+     <StatusBarHeader />
+     <Home/>
+    </>
+  );
+}
+
---
+++ src/components/StatusBarHeader/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StatusBar } from 'expo-status-bar'
+
+export function StatusBarHeader() {
+  return(
+    <StatusBar
+      style="light"
+    />
+  )
+}
